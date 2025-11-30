4fc53a061a4f23e60738ad1478f2725ae280dd6f
Sako M <43357548+sakomws@users.noreply.github.com>
2025-12-01 02:47:43 +0400

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://na4zg40otd.execute-api.us-east-1.amazonaws.com',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api',
   },
 }
-
-module.exports = nextConfig
